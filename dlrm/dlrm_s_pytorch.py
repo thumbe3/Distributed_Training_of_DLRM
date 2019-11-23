4d4c04d4b6c9aa9e4123ebe4794e05d19f4013a3
thumbe3 <43432401+thumbe3@users.noreply.github.com>
2019-11-22 20:56:34 -0600

    Update dlrm_s_pytorch.py

--- dlrm/dlrm_s_pytorch.py
+++ dlrm/dlrm_s_pytorch.py
@@ -894,7 +894,7 @@ if __name__ == "__main__":
         while k < args.nepochs:
             for j, (X, lS_o, lS_i, T) in enumerate(train_loader):
                 # early exit if nbatches was set by the user and has been exceeded
-                if j >= 10000:
+                if j >= int(10000*128/args.mini_batch_size):
                     break
                 '''
                 # debug prints
