adec2f4d8cc6aa8b73649806c59272bea298cadd
ushmal <ushmal@c4130-110133.wisc.cloudlab.us>
2019-12-05 19:03:29 -0600

    finalized clsuter bash script

--- dlrm/cluster_utils.sh
+++ dlrm/cluster_utils.sh
@@ -14,7 +14,7 @@ function start_cluster() {
     else
         echo "Create "
         echo "Copying the script to all the remote hosts."
-        mkdir -p output
+        mkdir -p $7
         echo "The output is logged to output/outpuslog-i.out, where i = 0,..2 are the VM numbers."
 
         CMD="python3 -W ignore dlrm_s_pytorch.py --use-gpu --data-partition --arch-sparse-feature-size=16 --arch-mlp-bot="23-512-256-64-16" --arch-mlp-top="512-256-1" --data-generation=yelp --loss-function=bce --round-targets=True --learning-rate=0.1  --test-freq 1000000 --print-freq=1024 --print-time" 
@@ -24,12 +24,12 @@ function start_cluster() {
         if [ "$1" = "single" ]; then
         
             new_cmd="${CMD} --world-size=1 --master_ip 10.10.1.1 --rank 0  ${ARGS}"
-            nohup ssh $node0 "killall -9 python3; cd $PATH1; $new_cmd"> output/$1-$2-$3-$4-$5-0.out 2>&1&
+            nohup ssh $node0 "killall -9 python3; cd $PATH1; $new_cmd"> $7/$1-$2-$3-$4-$5-0.out 2>&1&
         else
             new_cmd="${CMD} --world-size=2 --master_ip 10.10.1.1 --rank 0  ${ARGS}"
-            nohup ssh $node0  "killall -9 python3; cd $PATH1; $new_cmd"> output/$1-$2-$3-$4-$5-0.out 2>&1&
+            nohup ssh $node0  "killall -9 python3; cd $PATH1; $new_cmd"> $7/$1-$2-$3-$4-$5-0.out 2>&1&
             new_cmd="${CMD} --world-size=2 --master_ip 10.10.1.1 --rank 1  ${ARGS}"
-            nohup ssh $node1  "killall -9 python3; cd $PATH1; $new_cmd"> output/$1-$2-$3-$4-$5-1.out 2>&1&
+            nohup ssh $node1  "killall -9 python3; cd $PATH1; $new_cmd"> $7/$1-$2-$3-$4-$5-1.out 2>&1&
         fi
     fi
 }
--- dlrm/run.sh
+++ dlrm/run.sh
@@ -1,22 +1,22 @@
 source cluster_utils.sh
-cluster_mode=("cluster" "single" )
+cluster_mode=("cluster" "single")
 batch_size=("32" "64" "128" "512")
 interaction=("dot" "cat")
-ngpus=(2 3)
+ngpus=(1 2 3)
 async_mode=("True" "False")
 epochs=10
+dataset=$1
 
-
-
+mkdir -p $dataset
 for c_mode in "${cluster_mode[@]}"; do
 for bsize in "${batch_size[@]}"; do
 for inter in "${interaction[@]}"; do
 for gpu in "${ngpus[@]}"; do
 for s_mode in "${async_mode[@]}"; do
-  num_line="$(grep -nr 'Finished the program' output|wc -l)"
+  num_line="$(grep -nr 'Finished the program' $dataset|wc -l)"
   new_num_line=num_line
   
-  start_cluster $c_mode $gpu $s_mode $bsize $inter $epochs
+  start_cluster $c_mode $gpu $s_mode $bsize $inter $epochs $dataset 
   if [ $c_mode == "single" ]; then
 	  change=1
 	  else
@@ -27,23 +27,18 @@ for s_mode in "${async_mode[@]}"; do
   while [ $(($new_num_line-$num_line)) -lt $change ];
   do
   sleep 1
-  new_num_line="$(grep -nr 'Finished the program' output|wc -l)"
+  new_num_line="$(grep -nr 'Finished the program' $dataset|wc -l)"
   done
   killall -9 python3
 
-  git add output
-  git commit -m "Periodic commit"
+  git add $dataset
+  git commit -m "Periodic commit for $dataset"
   git push
   echo "GIT PUSHED"
-  break
   
 done
- break
 done
- break
 done
- break
 done
-break
 done
 
