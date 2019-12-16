6eb242d2831cb9e1455f5edd87de26ab4adb900d
ushmal <ushmal@clgpu001.clemson.cloudlab.us>
2019-12-16 01:49:34 -0500

    add wb to pickle

--- dlrm/dlrm_s_pytorch.py
+++ dlrm/dlrm_s_pytorch.py
@@ -1188,7 +1188,7 @@ if __name__ == "__main__":
         onnx.checker.check_model(dlrm_pytorch_onnx)
 
 import pickle
-out=open("emb"+str(rank)+".pickle")
+out=open("emb"+str(rank)+".pickle", "wb")
 pickle.dump(embed_global, out)
 
 print("Finished the program")
